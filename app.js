965a61922ed8686926ca26ec0a106ede5b3a3aad
Widy Guilias <widyguilias@gmail.com>
2022-12-14 12:26:34 +0700

    connect the app to database mongodb using mongoose library

--- app.js
+++ app.js
@@ -1,10 +1,32 @@
 const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
 
 const app = express()
 
-app.get('/', (req, res) => {
-	res.send('hello user')
-})
+app.use(express.json())
+app.use(express.urlencoded())
+
+const db = require('./app/models/')
+db.mongoose
+	.connect(db.url, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
+	.then(()  => {
+		console.log('Database connected')
+	})
+	.catch(err => {
+		console.log(err)
+		console.error('Database not connected')
+		process.exit()
+	})
+
+// app.get('/', (req, res) => {
+// 	res.send('hello user')
+// })
+
+require('./app/routes/expense.routes')(app)
 
 app.listen('8080', () => {
 	console.log('server is listening on port 8080')
